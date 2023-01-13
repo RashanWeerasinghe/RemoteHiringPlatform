c56e6f897225186d543fd1122aca560903c9cdfa
RashanWeerasinghe <malithrashan03@gmail.com>
2023-01-14 00:44:10 +0530

    first commit

---
+++ rhp/src/main/java/com/example/rhp/RhpApplication.java
@@ -0,0 +1,13 @@
+package com.example.rhp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhpApplication.class, args);
+	}
+
+}
---
+++ rhp/src/test/java/com/example/rhp/RhpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rhp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
